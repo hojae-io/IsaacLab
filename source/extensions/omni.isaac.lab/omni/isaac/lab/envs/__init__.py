f1816c3680d3d7827c55a5abb6afe6331f2cc514
hojae <hjlee201@mit.edu>
2024-08-13 12:57:08 -0400

    Add HumanoidVanillaEnv

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/__init__.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/__init__.py
@@ -43,3 +43,5 @@ from .manager_based_env import ManagerBasedEnv
 from .manager_based_env_cfg import ManagerBasedEnvCfg
 from .manager_based_rl_env import ManagerBasedRLEnv
 from .manager_based_rl_env_cfg import ManagerBasedRLEnvCfg
+
+from extensions.humanoid.task import HumanoidVanillaEnv
