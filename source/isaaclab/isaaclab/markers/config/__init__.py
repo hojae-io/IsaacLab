1b5b66dd8a9c4377a65af048e6bb8a983566dc83
hojae <hjlee201@mit.edu>
2026-04-11 13:23:42 -0400

    Add YELLOW_ORANGE and SKY_BLUE arrows

--- source/isaaclab/isaaclab/markers/config/__init__.py
+++ source/isaaclab/isaaclab/markers/config/__init__.py
@@ -102,6 +102,28 @@ GREEN_ARROW_X_MARKER_CFG = VisualizationMarkersCfg(
 )
 """Configuration for the green arrow marker (along x-direction)."""
 
+YELLOW_ORANGE_ARROW_X_MARKER_CFG = VisualizationMarkersCfg(
+    markers={
+        "arrow": sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/arrow_x.usd",
+            scale=(0.3, 0.3, 0.3),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(1.0, 0.71, 0.18)),
+        )
+    }
+)
+"""Configuration for the yellow orange arrow marker (along x-direction)."""
+
+SKY_BLUE_ARROW_X_MARKER_CFG = VisualizationMarkersCfg(
+    markers={
+        "arrow": sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/UIElements/arrow_x.usd",
+            scale=(0.3, 0.3, 0.3),
+            visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.239, 0.647, 1.0)),
+        )
+    }
+)
+"""Configuration for the sky blue arrow marker (along x-direction)."""
+
 PINK_ARROW_X_MARKER_CFG = VisualizationMarkersCfg(
     markers={
         "arrow": sim_utils.UsdFileCfg(
